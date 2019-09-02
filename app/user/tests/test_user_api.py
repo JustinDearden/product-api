db2ffe9d4925cad10a375c019fb2984f7af74fcf
Justin Dearden <deardenj@uwindsor.ca>
2019-09-02 17:21:01 -0400

    Updated User Create Test.

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -32,8 +32,8 @@ class PublicUserApiTests(TestCase):
 
         # expect an http 201 (created user)
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
-        # user = get_user_model().objects.get(**res.data)
-        # self.assertTrue(user.check_password(payload['password']))
+        user = get_user_model().objects.get(**res.data)
+        self.assertTrue(user.check_password(payload['password']))
         self.assertNotIn('password', res.data)
 
     def test_user_exists(self):
@@ -137,5 +137,5 @@ class PrivateUserApiTests(TestCase):
 
         self.user.refresh_from_db()
         self.assertEqual(self.user.name, payload['name'])
-        # self.assertTrue(self.user.check_password(payload['password']))
+        self.assertTrue(self.user.check_password(payload['password']))
         self.assertEqual(res.status_code, status.HTTP_200_OK)
